1f78452659966ff7c892a770ff5617d67908fe17
Copetan <copetanbird@gmail.com>
2021-02-14 15:40:52 -0800

    some refactoring
    I felt uncomfortable copying the entire forEachEnchantment() method to the helper so i just used access wideners to access both the method and the functional Consumer interface in the vanilla EnchantmentHelper class
    
    It might be hacky but it makes me feel better than copying mojang's code lol

--- src/main/java/carpetlunaaraddons/helpers/GetAttackDamageHelper.java
+++ src/main/java/carpetlunaaraddons/helpers/GetAttackDamageHelper.java
@@ -1,20 +1,17 @@
 package carpetlunaaraddons.helpers;
 
-import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.ImpalingEnchantment;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.apache.commons.lang3.mutable.MutableFloat;
 
 public class GetAttackDamageHelper
 {
 	public static float getAttackDamage(ItemStack stack, LivingEntity target) {
 		MutableFloat mutableFloat = new MutableFloat();
-		forEachEnchantment((enchantment, level) -> {
+		EnchantmentHelper.forEachEnchantment((enchantment, level) -> {
 			if (enchantment instanceof ImpalingEnchantment)
 				mutableFloat.add((target.getGroup() == EntityGroup.AQUATIC) || (target.isTouchingWaterOrRain())
 						? (float)level * 2.5F : 0.0F);
@@ -22,25 +19,5 @@ public class GetAttackDamageHelper
 				mutableFloat.add(enchantment.getAttackDamage(level,target.getGroup()));
 		},stack);
 		return mutableFloat.floatValue();
-	}
-
-	private static void forEachEnchantment(GetAttackDamageHelper.Consumer consumer, ItemStack stack) {
-		if (!stack.isEmpty()) {
-			ListTag listTag = stack.getEnchantments();
-
-			for(int i = 0; i < listTag.size(); ++i) {
-				String string = listTag.getCompound(i).getString("id");
-				int j = listTag.getCompound(i).getInt("lvl");
-				Registry.ENCHANTMENT.getOrEmpty(Identifier.tryParse(string)).ifPresent((enchantment) -> {
-					consumer.accept(enchantment, j);
-				});
-			}
-
-		}
-	}
-
-	@FunctionalInterface
-	interface Consumer {
-		void accept(Enchantment enchantment, int level);
 	}
 }
