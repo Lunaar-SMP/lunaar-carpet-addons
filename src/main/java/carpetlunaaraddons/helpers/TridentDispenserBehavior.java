c21dd5b295d97d00b9ff96eb5b0ca4f9e7deb483
whoImT <36664975+whoImT@users.noreply.github.com>
2021-11-03 16:17:25 +0100

    remove unnecessary code

--- src/main/java/carpetlunaaraddons/helpers/TridentDispenserBehavior.java
+++ src/main/java/carpetlunaaraddons/helpers/TridentDispenserBehavior.java
@@ -1,26 +1,16 @@
 package carpetlunaaraddons.helpers;
 
-import carpetlunaaraddons.CarpetLunaarSettings;
 import net.minecraft.block.dispenser.ProjectileDispenserBehavior;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
 
 public class TridentDispenserBehavior extends ProjectileDispenserBehavior
 {
-
-    @Override
-    protected void playSound(BlockPointer pointer) {
-        if (CarpetLunaarSettings.dispensersShootTridents)
-            super.playSound(pointer);
-        else
-            pointer.getWorld().syncWorldEvent(1000, pointer.getPos(), 0);
-    }
 
     @Override
     protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
