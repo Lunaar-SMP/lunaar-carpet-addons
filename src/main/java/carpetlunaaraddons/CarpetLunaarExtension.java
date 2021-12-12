14513cb5270ed276a8ba4929cfc69cbc460a4081
Copetan <copetanbird@gmail.com>
2021-12-11 19:28:03 -0800

    Add Fabric ASM
    phantom shenanigans; there's a reason why this is a separate branch

--- src/main/java/carpetlunaaraddons/CarpetLunaarExtension.java
+++ src/main/java/carpetlunaaraddons/CarpetLunaarExtension.java
@@ -2,14 +2,21 @@ package carpetlunaaraddons;
 
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
+import com.chocohead.mm.api.ClassTinkerers;
 import com.mojang.brigadier.CommandDispatcher;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.ServerCommandSource;
 
-public class CarpetLunaarExtension implements CarpetExtension, ModInitializer
+public class CarpetLunaarExtension implements CarpetExtension
 {
-    @Override
-    public void onInitialize() {
+    public static void earlyRiser() {
+        String name = FabricLoader.getInstance().getMappingResolver()
+                .mapClassName("intermediary", "net.minecraft.class_1311");
+        ClassTinkerers.enumBuilder(name, String.class, int.class, boolean.class, boolean.class, int.class)
+                .addEnum("PHANTOM", "phantom", 15, false, false, 128).build();
+    }
+
+    public static void initializeExtension() {
         CarpetServer.manageExtension(new CarpetLunaarExtension());
     }
 
